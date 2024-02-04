5ac598f4b6953eff4b30d371fb2427924862d3be
neodev12 <145462538+neodev12@users.noreply.github.com>
2024-02-04 12:51:52 -0800

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,43 @@
+import random
+import numpy as np
+# #q1
+# even_num=np.arange(30,71,2)
+# print(even_num)
+# A= np.array([1,2,4])
+# B=np.array([2,4,5])
+# result = np.cross(A,B)
+# print(result)
+#q4
+#A= np.array([1,8,4])
+#B=np.array([7,4,5])
+#H = np.cross(A,B)
+#print(H)
+#q4
+# W=np.random.random((3,3,3))
+# print(W)
+#q5
+# x=np.random.random((5,5))
+# print("THE ARRAY")
+# print(x)
+# xmin ,xmax =x.min(),x.max()
+# print("minimum value " "maximum value")
+# print(xmin,xmax)
+# random_n1= np.random.normal(size=1000)
+# print("print random numbers from a standard normal distribution")
+# print(random_n1)
+#q7
+ushna= np.random.randn(1000)
+print("Average of the array elements:")
+mean = ushna.mean()
+print(mean)
+print("Standard deviation of the array elements:")
+std = ushna.std()
+print(std)
+print("Variance of the array elements:")
+var = ushna.var()
+print(var)
+
+
+
+
+
